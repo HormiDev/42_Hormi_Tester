0ef571e9afb1a2b98abf2602629a10fd0779ebe5
HormiDev <ivanddiegomegia@gmail.com>
2025-01-29 03:50:40 +0100

    Actualizar argumentos de prueba en Hormi_Tester.sh para incluir funciones de la biblioteca estándar y eliminar funciones no necesarias

--- Hormi_Tester.sh
+++ Hormi_Tester.sh
@@ -39,7 +39,7 @@ print_header() {
 }
 
 execute_test() {
-	ARGUMENTS_FOR_LIBFT=("libft" "ft_memset" "ft_bzero" "ft_memcpy" "ft_memccpy" "ft_memmove" "ft_memchr" "ft_memcmp" "ft_strlen" "ft_strdup" "ft_strcpy" "ft_strncpy" "ft_strcat" "ft_strncat" "ft_strlcat" "ft_strchr" "ft_strrchr" "ft_strstr" "ft_strnstr" "ft_strcmp" "ft_strncmp" "ft_atoi" "ft_isalpha" "ft_isdigit" "ft_isalnum" "ft_isascii" "ft_isprint" "ft_toupper" "ft_tolower" "ft_memalloc" "ft_memdel" "ft_strnew" "ft_strdel" "ft_strclr" "ft_striter" "ft_striteri" "ft_strmap" "ft_strmapi" "ft_strequ" "ft_strnequ" "ft_strsub" "ft_strjoin" "ft_strtrim" "ft_strsplit" "ft_itoa" "ft_putchar" "ft_putstr" "ft_putendl" "ft_putnbr" "ft_putchar_fd" "ft_putstr_fd" "ft_putendl_fd" "ft_putnbr_fd")
+	ARGUMENTS_FOR_LIBFT=("libft" "isalpha" "isdigit" "isalnum" "isascii" "isprint" "toupper" "tolower" "strlen" "memset" "bzero" "memcpy" "memccpy" "memmove" "memchr" "memcmp" "strlcpy" "strlcat" "strchr" "strrchr" "strncmp" "strnstr" "atoi" "calloc" "strdup")
 	local arg="$1"
 	if [[ " ${ARGUMENTS_FOR_LIBFT[@],,} " =~ " ${arg,,} " ]]; then
 		echo -e "${GREEN}Executing tests for ${arg}...${NC}"
